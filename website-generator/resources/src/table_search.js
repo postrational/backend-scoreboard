11d8eb02a8e72e6f579c10625a96e9db2ca478e2
Ewa21 <ewa.tusien@intel.com>
2021-02-16 13:00:10 +0100

    Add filter to ops and format code.

--- website-generator/resources/src/table_search.js
+++ website-generator/resources/src/table_search.js
@@ -4,27 +4,27 @@
 
 // Table search
 (function () {
-  function onSearch (inputId, tableId) {
+  function onSearch(inputId, tableId, tableType) {
     const input = document.getElementById(inputId);
     const filter = input.value.toLowerCase();
     const table = document.getElementById(tableId);
     const testNames = table.getElementsByClassName('testName');
     const allBackendsResults = table.getElementsByClassName('backendResults');
-    const showPassed = document.getElementById('showPassed').checked;
-    const showFailed = document.getElementById('showFailed').checked;
+    const showPassed = document.getElementById('showPassed' + tableType).checked;
+    const showFailed = document.getElementById('showFailed' + tableType).checked;
     let testStatus = ''
 
-    if (showPassed && !showFailed){
+    if (showPassed && !showFailed) {
       testStatus = 'Passed';
     }
-    else if (showFailed && !showPassed){
+    else if (showFailed && !showPassed) {
       testStatus = "Failed"
     }
     for (let i = 0; i < testNames.length; i++) {
       const testNameText = testNames[i].textContent || testNames[i].innerText;
       for (let j = 0; j < allBackendsResults.length; j++) {
         const backendResults = allBackendsResults[j].getElementsByClassName('testResult');
-        if (testNameText.toLowerCase().indexOf(filter) < 0 ||  (testStatus !== '' && backendResults[i].textContent.trim() !== testStatus)) {
+        if (testNameText.toLowerCase().indexOf(filter) < 0 || (testStatus !== '' && backendResults[i].textContent.trim() !== testStatus)) {
           testNames[i].style.display = 'none';
           backendResults[i].style.display = 'none';
         } else {
