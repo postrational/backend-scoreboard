99abe564055aebd569457af1baf349ed97f26047
Ewa21 <ewa.tusien@intel.com>
2021-02-16 10:49:16 +0100

    Add filter passed/failed for tests.

--- website-generator/resources/src/table_search.js
+++ website-generator/resources/src/table_search.js
@@ -10,12 +10,21 @@
     const table = document.getElementById(tableId);
     const testNames = table.getElementsByClassName('testName');
     const allBackendsResults = table.getElementsByClassName('backendResults');
+    const showPassed = document.getElementById('showPassed').checked;
+    const showFailed = document.getElementById('showFailed').checked;
+    let testStatus = ''
 
+    if (showPassed && !showFailed){
+      testStatus = 'Passed';
+    }
+    else if (showFailed && !showPassed){
+      testStatus = "Failed"
+    }
     for (let i = 0; i < testNames.length; i++) {
       const testNameText = testNames[i].textContent || testNames[i].innerText;
       for (let j = 0; j < allBackendsResults.length; j++) {
         const backendResults = allBackendsResults[j].getElementsByClassName('testResult');
-        if (testNameText.toLowerCase().indexOf(filter) < 0) {
+        if (testNameText.toLowerCase().indexOf(filter) < 0 ||  (testStatus !== '' && backendResults[i].textContent.trim() !== testStatus)) {
           testNames[i].style.display = 'none';
           backendResults[i].style.display = 'none';
         } else {
