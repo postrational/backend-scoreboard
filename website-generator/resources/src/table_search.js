77cd400ee21f4b5da5c5f71cc6999e5d11c16543
Ewa21 <ewa.tusien@intel.com>
2021-02-17 20:53:05 +0100

    Add docstring and make checkboxes selected.

--- website-generator/resources/src/table_search.js
+++ website-generator/resources/src/table_search.js
@@ -2,7 +2,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// Table search
+/**
+ * Search tests/operators by name and result.
+ *
+ * @param {String} inputId The id of an input field which contains a search name 
+ * @param {String} tableId The id of a table with tests/operators
+ * @param {String} tableType The type of entries in the table. Either 'Tests' or 'Ops' 
+ */
 (function () {
   function onSearch(inputId, tableId, tableType) {
     const input = document.getElementById(inputId);
@@ -18,14 +24,14 @@
       testStatus = 'passed';
     }
     else if (showFailed && !showPassed) {
-      testStatus = "failed";
+      testStatus = 'failed';
     }
     for (let i = 0; i < testNames.length; i++) {
       const testNameText = testNames[i].textContent || testNames[i].innerText;
       for (let j = 0; j < allBackendsResults.length; j++) {
         const backendResults = allBackendsResults[j].getElementsByClassName('testResult');
         const testResult = backendResults[i].textContent.trim().toLowerCase();
-        if (testNameText.toLowerCase().indexOf(filter) < 0 || (testStatus &&  testResult !== testStatus)) {
+        if (testNameText.toLowerCase().indexOf(filter) < 0 || (testStatus && testResult !== testStatus)) {
           testNames[i].style.display = 'none';
           backendResults[i].style.display = 'none';
         } else {
